76ade1015cd02ef38c1d8dd59de9279c2ac24f06
Matthew Pomes <the10thwiz@gmai.com>
2020-06-04 09:51:08 -0500

    Write Example code for library

---
+++ examples/simple.rs
@@ -0,0 +1,17 @@
+use simple_pdf::{graphics, Page, PDF};
+use std::fs::File;
+
+fn main() -> std::io::Result<()> {
+    let mut pdf = PDF::new();
+    let mut page = Page::new(); // Page builder
+    page.add(
+        &graphics::Path::from((10f64, 10f64))
+            .line_to((200f64, 200f64))
+            .rect((10f64, 10f64, 190f64, 190f64))
+            .stroke(graphics::Color::red()),
+    );
+    pdf.add_page(page);
+    
+    let mut output = File::create("test3")?;
+    pdf.write(&mut output)
+}

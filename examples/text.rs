7d33634e201b4f0961b8368cb11312477d26567c
the10thwiz <the10thwiz@gmail.com>
2020-06-04 14:01:32 -0500

    Write example/text.rs for better text API

--- examples/text.rs
+++ examples/text.rs
@@ -3,10 +3,11 @@ use std::fs::File;
 
 fn main() -> std::io::Result<()> {
     let mut pdf = PDF::new();
-    let mut page = Page::new(); // Page builder
+    let mut page = Page::new();
     page.add(
         graphics::Text::new(graphics::Font::times_new_roman(), 12f64)
             .move_to((100f64, 100f64))
+            .fill(graphics::Color::red())
             .text("Hello World!"),
     );
     pdf.add_page(page);
